cc7c67f774bfab2f51a4e551daf12d0a84671619
Jino1211 <yone543@gmail.com>
2021-01-24 12:47:30 +0200

    final

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const ul = document.querySelector('ul');
-const span = document.querySelector('span');
-const button = document.querySelector('button');
+const input= document.querySelector('input');
+const addButton = document.querySelector('button');
+
+addButton.onclick = function () {
+    let currentValue = input.value;
+    input.value = '';
+
+    const list = document.createElement('li');
+    const spanList = document.createElement('span');
+    const deleteButton = document.createElement('button');
+
+    list.appendChild(spanList);
+    list.appendChild(deleteButton);
+    spanList.textContent = currentValue;
+    deleteButton.textContent = 'Delete';
+    ul.appendChild(list);
+
+    deleteButton.onclick = function() {
+    ul.removeChild(list);
+    }
+}
+input.focus();
